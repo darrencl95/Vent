8b0586c037d21e8bbafeb9c0fa15e8407c071fe5
Darren Leung <darren.leung@gatech.edu>
2017-03-05 02:57:09 -0500

    initial commit

---
+++ app/src/main/java/com/pingus/vent/Controller/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.pingus.vent.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pingus.vent.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
