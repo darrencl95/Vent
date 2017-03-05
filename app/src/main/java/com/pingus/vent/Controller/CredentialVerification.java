622bb6808689e2a6a27cc5bff7a44bdb58f9240e
Darren Leung <darren.leung@gatech.edu>
2017-03-05 03:17:02 -0500

    Added basic login view with authentication and database test

---
+++ app/src/main/java/com/pingus/vent/Controller/CredentialVerification.java
@@ -0,0 +1,35 @@
+package com.pingus.vent.Controller;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Darren on 3/5/2017.
+ */
+
+public class CredentialVerification {
+
+    private static final Pattern p = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x07\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x07\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
+
+    public static boolean verifyEmail(String email) {
+        Matcher m = p.matcher(email);
+        return m.matches();
+    }
+
+    public static String verifyPassword(String password) {
+        String message = "";
+        boolean length = password.length() >= 8;
+        boolean hasUppercase = !password.equals(password.toLowerCase());
+        boolean hasLowercase = !password.equals(password.toUpperCase());
+        boolean hasNumber = password.matches(".*\\d+.*");
+
+        message += (!length) ? "Password is too short" : "";
+        message += (!hasUppercase) ? "\nPassword does not contain an uppercase" : "";
+        message += (!hasLowercase) ? "\nPassword does not contain a lowercase" : "";
+        message += (!hasNumber) ? "\nPassword does not contain a number" : "";
+
+        return message;
+    }
+}
+
+
--- app/src/main/java/com/pingus/vent/Controller/LoginActivity.java
+++ app/src/main/java/com/pingus/vent/Controller/LoginActivity.java
@@ -1,15 +1,159 @@
 package com.pingus.vent.Controller;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.pingus.vent.R;
+import com.pingus.vent.Model.User;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+    // declare UI components
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button loginBtn;
+    private Button registerBtn;
+    private TextView textViewForgot;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        loginBtn = (Button) findViewById(R.id.buttonLogin);
+        registerBtn = (Button) findViewById(R.id.registerButton);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("Pickup", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d("Pickup", "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+                signIn(email, password);
+            }
+        });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+                if (!CredentialVerification.verifyEmail(email)) {
+                    Toast.makeText(getBaseContext(), "Invalid Email",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String message = CredentialVerification.verifyPassword(password);
+                if (!message.isEmpty()) {
+                    Toast.makeText(getBaseContext(), message, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                createAccount(email, password);
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void createAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("Pickup", "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                            if (user != null) {
+                                DatabaseReference myRef = database.getReference(user.getUid());
+                                User u = new User("trollmaster6969","Alexandre","Locquet");
+                                myRef.setValue(u);
+                            }
+                            Toast.makeText(getBaseContext(), "Authentication succeeded",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.d("Firebase",task.getException().getMessage().toString());
+                            Toast.makeText(getBaseContext(), "Authentication failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("Pickup", "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getBaseContext(), "Authentication succeeded",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.w("Pickup", "signInWithEmail:failed", task.getException());
+                            Toast.makeText(getBaseContext(), "Authentication failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
