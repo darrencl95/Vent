7ad6d4172bc92f7ee1ebcbe208f73f641d41f19d
smantri7 <smantri7@gatech.edu>
2017-04-03 23:30:46 -0400

    Fixed Quote Error

--- app/src/main/java/com/pingus/vent/Controller/HomeFragment.java
+++ app/src/main/java/com/pingus/vent/Controller/HomeFragment.java
@@ -20,7 +20,6 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.forismastic.Forismatic;
 import com.pingus.vent.Model.QuoteDialog;
 import com.pingus.vent.R;
 /**
