8f9dd9d325e73d9bc2217e012b63b9381b252a5e
smantri7 <smantri7@gatech.edu>
2017-04-06 17:45:24 -0400

    Refactored About Us to Profile Activity

--- app/src/main/java/com/pingus/vent/Controller/ProfileActivity.java
+++ app/src/main/java/com/pingus/vent/Controller/ProfileActivity.java
@@ -6,13 +6,13 @@ import android.view.MenuItem;
 
 import com.pingus.vent.R;
 
-public class AboutUsActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about_us);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_profile);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
